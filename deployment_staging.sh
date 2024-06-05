d8903a1756bdc63c083fc2d30f2e2035e3a6d339
Guillaume <astier.gui@gmail.com>
2024-06-05 10:57:09 +0200

    Create deployment_staging.sh

---
+++ deployment_staging.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Only work for python application on o2switch website provider
+
+push_staging_env() {
+	local PYTHON_VERSION="3.11"
+	local APP_FILENAME_PATH="django.yosu0027.odns.fr"
+	local APP_ROOT_PATH="/home/yosu0027"
+
+	source $APP_ROOT_PATH/virtualenv/$APP_FILENAME_PATH/$PYTHON_VERSION/bin/activate
+	cd APP_ROOT_PATH/APP_FILENAME_PATH
+	git pull origin main
+	pip cache purge
+}
+
+push_staging_env()
