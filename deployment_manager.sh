1fb425cbef28e761a746c489af64662b9439ce87
Guillaume <astier.gui@gmail.com>
2024-06-05 12:02:54 +0200

    Update deployment_manager.sh

--- deployment_manager.sh
+++ deployment_manager.sh
@@ -2,7 +2,7 @@
 
 deployment_manager_watch() {
   watch -n10 "watch -t -g ls --full-time /home/yosu0027/deployment/staging >/dev/null && /home/yosu0027/deployment_staging.sh >> ~/logs/django.yosu0027.odns.fr/deployment.log" &
-  watch -n5 "watch -t -g ls --full-time /home/yosu0027/deployment/production >/dev/null && /home/yosu0027/deployment_production/sh >> ~/logs/jak-pot.com/deployment.log"
+  watch -n5 "watch -t -g ls --full-time /home/yosu0027/deployment/production >/dev/null && /home/yosu0027/deployment_production.sh >> ~/logs/jak-pot.com/deployment.log"
 }
 
 deployment_manager_watch
